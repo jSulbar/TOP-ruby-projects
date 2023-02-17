3a735f6d8ab3fbb3385af07e77d25645731f5a44
jSulbar <diosmiolacreatura@gmail.com>
2023-02-17 02:18:32 -0400

    Finish hangman

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -6,17 +6,19 @@ class Hangman
   include Playable
 
   def initialize(wordlist)
-    @wordlist = wordlist
-    @secret_word = randomized_word.upcase
+    # @wordlist = wordlist
+    @secret_word = wordlist.sample.chomp.upcase
     @word_progress = Array.new(@secret_word.length, '_')
 
     @hangman_count = 0
     @turn_limit = @secret_word.length + @secret_word.length / 2
+    @GAME_NAME = self.class.name
   end
 
-  private
+  attr_accessor :wordlist, :secret_word, :word_progress, :hangman_count,
+                :turn_limit
 
-  attr_accessor :wordlist, :secret_word, :word_progress
+  private
 
   def print_gamestate
     print_hangman
@@ -32,19 +34,22 @@ class Hangman
   end
 
   def process_input(input)
+    return save_and_exit if input[0] == '#'
+
     input.match(/^[[:alpha:]]+$/) ? input[0] : process_input(gets.chomp)
   end
 
   def match_answer(ans, secret_word)
     if secret_word.include?(ans)
-      @word_progress[secret_word =~ Regexp.new(ans)] = ans
-      match_answer(ans, secret_word.sub(ans, ''))
+      @word_progress[secret_word.index(ans)] = ans
+      match_answer(ans, secret_word.sub(ans, '_'))
     end
   end
 
   def process_turn
     ans = get_ply_input(
-      "Input a letter (#{@turn_limit - @hangman_count} turns remaining): "
+      "Input a letter (#{(@turn_limit - @hangman_count) + 1} turns remaining)",
+      'To save the game and exit, use #: '
     )
     match_answer(ans.upcase, @secret_word.to_s)
   end
@@ -64,6 +69,8 @@ class Hangman
 end
 
 $DEBUG = false
+# wordlist too long to see error on terminal,
+# redirect errors to a text file
 $stderr.reopen('err.txt', 'w') if $DEBUG
 
 wordlist = File.open('google-10000-english-no-swears.txt', 'r').readlines

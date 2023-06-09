ef0b7be8e26d97ddb9a5c0965d131e0a91c7e162
jSulbar <diosmiolacreatura@gmail.com>
2023-06-09 04:39:13 -0400

    Write initial tests
    Describe #initialize and declare incomplete method tests

--- connect-four/spec/board_spec.rb
+++ connect-four/spec/board_spec.rb
@@ -1,4 +1,33 @@
 require_relative '../lib/board'
 
 describe Board do
+  describe '#initialize' do
+    subject(:board_init) { described_class.new }
+
+    it 'creates a 7x6 size 2d array' do
+      slot_count = board_init.slots.flatten.length
+      expect(slot_count).to eq(7 * 6)
+    end
+  end
+
+  describe '#won?' do
+  end
+
+  describe '#four_diagonal' do
+  end
+
+  describe '#four_row' do
+  end
+
+  describe '#four_column' do
+  end
+
+  describe '#drop' do
+  end
+
+  describe '#full?' do
+  end
+
+  describe '#empty?' do
+  end
 end

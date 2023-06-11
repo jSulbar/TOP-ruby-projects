23db7f2f2cc5aa9d924caf3880cbaf29d7504672
jSulbar <diosmiolacreatura@gmail.com>
2023-06-11 10:53:38 -0400

    Add #game_over, #process_input
    Add 2 of the methods required by Playable module

--- connect-four/lib/connect_four.rb
+++ connect-four/lib/connect_four.rb
@@ -11,4 +11,14 @@ class ConnectFour
     @board = board
     @turn_queue = %w[P1 P2]
   end
+
+  def game_over?
+    @board.won? || @board.full?
+  end
+
+  def process_input(user_input)
+    user_input = user_input.to_i
+    [user_input - 1, @turn_queue.first] unless user_input > @board.slots[0].length ||
+                                               user_input < 1
+  end
 end

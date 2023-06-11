eba4ae51b68bcbd8e671baab21da31123342f278
jSulbar <diosmiolacreatura@gmail.com>
2023-06-11 09:43:40 -0400

    Describe #process_input
    Write rspec tests for ConnectFour#process_input.

--- connect-four/spec/connect_four_spec.rb
+++ connect-four/spec/connect_four_spec.rb
@@ -30,4 +30,28 @@ describe ConnectFour do
       end
     end
   end
+
+  describe '#process_input' do
+    subject(:connect_input) { described_class.new }
+
+    before do
+      @player = 'P1'
+    end
+
+    context 'with valid input' do
+      it 'returns the chosen column and current player' do
+        expect(connect_input.process_input(3, @player)).to eq([3, @player])
+      end
+    end
+
+    context 'with invalid input' do
+      it 'returns nil when column out of range' do
+        expect(connect_input.process_input(9, @player)).to be_nil
+      end
+
+      it 'returns nil when column is negative' do
+        expect(connect_input.process_input(-4, @player)).to be_nil
+      end
+    end
+  end
 end

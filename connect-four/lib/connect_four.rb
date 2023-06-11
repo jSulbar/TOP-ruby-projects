33c773d15b9053fcf37e37fcd5efed6dcf7b0fa9
jSulbar <diosmiolacreatura@gmail.com>
2023-06-11 09:53:43 -0400

    Write #initialize, require Board class

--- connect-four/lib/connect_four.rb
+++ connect-four/lib/connect_four.rb
@@ -1,6 +1,11 @@
 require_relative '../../lib/playable'
+require_relative './board'
 
 # Connect-four class for playing... connect-four.
 class ConnectFour
   include Playable
+
+  def initialize(board = Board.new)
+    @board = board
+  end
 end

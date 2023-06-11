b917fcd4b78c558277dccc2d27649a799bf09ec6
jSulbar <diosmiolacreatura@gmail.com>
2023-06-11 09:19:31 -0400

    Describe #game_over?
    Write rspec tests for ConnectFour#game_over?

--- connect-four/spec/connect_four_spec.rb
+++ connect-four/spec/connect_four_spec.rb
@@ -1,4 +1,33 @@
 require_relative '../lib/connect_four'
 
 describe ConnectFour do
+  describe '#game_over?' do
+    context 'with a winning player' do
+      let(:win_board) { double(won?: true) }
+      subject(:connect_end_win) { described_class.new(win_board) }
+
+      it 'returns true' do
+        expect(connect_end_win).to be_game_over
+      end
+    end
+
+    context 'with no winning player' do
+      context 'when the board is full' do
+        let(:full_board) { double(full?: true) }
+        subject(:connect_end_full) { described_class.new(full_board) }
+
+        it 'returns true' do
+          expect(connect_end_full).to be_game_over
+        end
+      end
+
+      context 'when slots are still available' do
+        subject(:connect_end) { described_class.new }
+
+        it 'returns false' do
+          expect(connect_end).not_to be_game_over
+        end
+      end
+    end
+  end
 end

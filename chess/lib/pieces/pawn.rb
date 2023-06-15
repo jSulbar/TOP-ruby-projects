54cb3251865c79136bfabad6336e3ddb7076afc6
jSulbar <diosmiolacreatura@gmail.com>
2023-06-15 12:28:36 -0400

    Implement #diagonal_pieces

--- chess/lib/pieces/pawn.rb
+++ chess/lib/pieces/pawn.rb
@@ -15,4 +15,11 @@ class Pawn
   def front(coord)
     @color == :white ? coord * -1 : coord
   end
+
+  def diagonal_pieces(pos, board)
+    [[pos[0] + front(1), pos[1] + 1],
+     [pos[0] + front(1), pos[1] - 1]].filter do |attack|
+       board.enemy_piece?(@color, *attack)
+     end
+  end
 end

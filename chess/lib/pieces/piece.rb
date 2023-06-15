747473c92e10e7d031b67288482b55a0ea0e3843
jSulbar <diosmiolacreatura@gmail.com>
2023-06-15 05:24:11 -0400

    Create generic piece mixin
    Outlines some general functionality for piece classes.

---
+++ chess/lib/pieces/piece.rb
@@ -0,0 +1,26 @@
+require './lib/movement_pattern'
+
+# Generic chesspiece class
+module Piece
+  def to_s
+    @char[@color]
+  end
+
+  def available_tiles(pos, board)
+    res = []
+    @move_list.each do |move|
+      if @continuous_movement
+        res += move.follow_until(pos) { |follow| board.tile_tuple(*follow).nil? }
+      else
+        res.push(move.from_pos(pos))
+      end
+    end
+    res
+  end
+
+  def make_movelist(min_offsets)
+    min_offsets.map do |offset|
+      MovementPattern.new(offset)
+    end
+  end
+end

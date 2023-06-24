396264ec9b51c63ed3b258bf80a41d1fedebcf28
jSulbar <diosmiolacreatura@gmail.com>
2023-06-24 08:02:49 -0400

    Split functionality of #available_tiles
    Create #followup_move to make #available_tiles more readable. Also add attribute accessors.

--- chess/lib/pieces/piece.rb
+++ chess/lib/pieces/piece.rb
@@ -2,23 +2,36 @@ require './lib/movement_pattern'
 
 # Generic chesspiece class
 module Piece
-  attr_accessor :notation
+  attr_accessor :notation, :color
 
   def to_s
     @char[@color]
+  end
+
+  def followup_move(pos, board, move)
+    if @continuous_movement
+      result = move.follow_until(pos) do |follow_up|
+        !board.can_move_to?(@color, *follow_up) ||
+          board.enemy_piece?(@color, *follow_up)
+      end
+
+      result.filter { |tile| board.can_move_to?(@color, *tile) }
+    else
+      result = move.from_pos(pos)
+      result if board.can_move_to?(@color, *result)
+    end
   end
 
   def available_tiles(pos, board)
     res = []
     @move_list.each do |move|
       if @continuous_movement
-        res += move.follow_until(pos) { |follow| board.tile_tuple(*follow).nil? }
+        res += followup_move(pos, board, move)
       else
-        # TODO: Missing condition
-        res.push(move.from_pos(pos))
+        res.push(followup_move(pos, board, move))
       end
     end
-    res
+    res.compact
   end
 
   def make_movelist(min_offsets)
